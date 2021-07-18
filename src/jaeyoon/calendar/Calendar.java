330e2495d9a4d8709d3fdb74210bcbc32474718f
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-07-18 23:28:44 +0900

    1

--- src/jaeyoon/calendar/Calendar.java
+++ src/jaeyoon/calendar/Calendar.java
@@ -1,6 +1,5 @@
 package jaeyoon.calendar;
 
-
 public class Calendar {
     private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -13,37 +12,73 @@ public class Calendar {
         return LEAP_MAX_DAYS[month - 1];
     }
 
-    public void printCalendar(int year, int month) {
+    public int getNumOfDays(int year, int month) {
+        int preYear = year - 1;
+        int numOfDays = preYear * 365 + (preYear / 4 - preYear / 100 + preYear / 400);
+        boolean leafYear = ((year % 4) == 0) && (((year % 100) != 0) || ((year % 400) == 0));
+
+        for (int i = 0; i < month - 1; i++) {
+            numOfDays += MAX_DAYS[i];
+        }
+
+        if (month >= 3 && leafYear) {
+            numOfDays++;
+        }
+
+        numOfDays++; //입력받은 월의 1일을 계산하므로 + 1
+        return numOfDays;
+    }
+
+    public void printCalendar(int year, int month, int days) {
         System.out.printf("  << %4d년%3d월 >>\n", year, month);
         System.out.println(" SU MO TU WE TH FR SA");
         System.out.println(" --------------------");
 
+        //get weekday automatically
+        int weekDay = days % 7;
+
+        for (int i = 0; i < weekDay; i++) {
+            System.out.print("   ");
+        }
+
         int maxDay = getMaxDaysOfMonth(month);
         int leapMaxDay = getLeapMaxDaysOfMonth(month);
 
-        if (isLeapYear(year)) {
-            printCalendar(leapMaxDay);
+        if (check(year)) {
+            printCalendar(leapMaxDay, weekDay);
         } else {
-            printCalendar(maxDay);
+            printCalendar(maxDay, weekDay);
         }
     }
 
-    private boolean isLeapYear(int year) {
-        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+    private boolean check(int year) {
+        boolean leafYear = ((year % 4) == 0) && (((year % 100) != 0) || ((year % 400) == 0));
+
+        if (leafYear) {
             return true;
         } else {
             return false;
         }
     }
 
-    private void printCalendar(int day) {
-        for (int i = 1; i <= day; i++) {
+    private void printCalendar(int day, int weekDay) {
+        int count = 7 - weekDay;
+
+        //print first line
+        for (int i = 1; i <= count; i++) {
             System.out.printf("%3d", i);
-            if (i % 7 == 0) {
+        }
+        System.out.println();
+
+        //print second line
+        for (int i = count + 1; i <= day; i++) {
+            System.out.printf("%3d", i);
+            if (weekDay == 0 && i % 7 == 0) {
+                System.out.println();
+            } else if (i % 7 == count) {
                 System.out.println();
             }
         }
         System.out.println();
     }
-
 }
--- src/jaeyoon/calendar/Prompt.java
+++ src/jaeyoon/calendar/Prompt.java
@@ -4,49 +4,79 @@ import java.util.Scanner;
 
 public class Prompt {
 
-    private final static String PROMPT = "cal> ";
+    public static void main(String[] args) {
+        Prompt p = new Prompt();
+        p.runPrompt();
+    }
+
+    /*
+     * param week : 요일명
+     * return : 0 ~ 6 (0 = 일요일, 6 = 토요일)
+     * */
+//    public int parseDay(String week) {
+//        if (week.equals("su")) {
+//            return 0;
+//        } else if (week.equals("mo")) {
+//            return 1;
+//        } else if (week.equals("tu")) {
+//            return 2;
+//        } else if (week.equals("we")) {
+//            return 3;
+//        } else if (week.equals("th")) {
+//            return 4;
+//        } else if (week.equals("fr")) {
+//            return 5;
+//        } else if (week.equals("sa")) {
+//            return 6;
+//        } else {
+//            return 0;
+//        }
+//    }
 
     public void runPrompt() {
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
 
-
         while (true) {
-            int year = setYear(scanner);
-            int month = setMonth(scanner);
-            if (month == -1) {
-                break;
-            }
-            if (month > 12) {
-                System.out.println("5252~ 그런 월은 없다구~");
-                continue;
-            }
-            cal.printCalendar(year, month);
+            int year = getYear(scanner);
+            if (checker(year)) break;
+
+            int month = getMonth(scanner);
+            if (check(month)) continue;
+
+            int days = cal.getNumOfDays(year, month);
+
+            cal.printCalendar(year, month, days);
         }
         System.out.println("Bye~");
     }
 
 
-    private int setMonth(Scanner scanner) {
-        int month;
+    private boolean check(int month) {
+        if (month > 12 || month < 1) {
+            System.out.println("5252~ 그런 월은 없다구~");
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checker(int exit) {
+        if (exit == -1) {
+            return true;
+        }
+        return false;
+    }
+
+    private int getMonth(Scanner userInput) {
         System.out.println("달을 입력하세요.");
-        System.out.print(PROMPT);
-        month = scanner.nextInt();
-        return month;
+        System.out.print("MONTH> ");
+        return userInput.nextInt();
     }
 
 
-    private int setYear(Scanner scanner) {
-        int year;
-        System.out.println("몇년도 달력인가요?");
-        System.out.print(PROMPT);
-        year = scanner.nextInt();
-        return year;
-    }
-
-
-    public static void main(String[] args) {
-        Prompt p = new Prompt();
-        p.runPrompt();
+    private int getYear(Scanner userInput) {
+        System.out.println("몇년도 달력인가요?(exit : -1)");
+        System.out.print("YEAR> ");
+        return userInput.nextInt();
     }
 }
