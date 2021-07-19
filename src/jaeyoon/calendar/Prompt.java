35fd3a87ebcf5e6a7adb78da9926ad57783230b5
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-07-20 00:31:59 +0900

    Update Prompt.java

--- src/jaeyoon/calendar/Prompt.java
+++ src/jaeyoon/calendar/Prompt.java
@@ -1,6 +1,6 @@
 package jaeyoon.calendar;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Prompt {
 
@@ -9,46 +9,66 @@ public class Prompt {
         p.runPrompt();
     }
 
-    /*
-     * param week : 요일명
-     * return : 0 ~ 6 (0 = 일요일, 6 = 토요일)
-     * */
-//    public int parseDay(String week) {
-//        if (week.equals("su")) {
-//            return 0;
-//        } else if (week.equals("mo")) {
-//            return 1;
-//        } else if (week.equals("tu")) {
-//            return 2;
-//        } else if (week.equals("we")) {
-//            return 3;
-//        } else if (week.equals("th")) {
-//            return 4;
-//        } else if (week.equals("fr")) {
-//            return 5;
-//        } else if (week.equals("sa")) {
-//            return 6;
-//        } else {
-//            return 0;
-//        }
-//    }
-
     public void runPrompt() {
+        displayInputMessage();
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
+        String delimeter = "-";
 
         while (true) {
-            int year = getYear(scanner);
-            if (checker(year)) break;
+            System.out.println("명령(1, 2, 3, h, q)");
+            String cmd = scanner.next();
+            if (cmd.equals("1")) cmdRegister();
+            else if (cmd.equals("2")) cmdSearch();
+            else if (cmd.equals("3")) printCal(scanner, delimeter, cal);
+            else if (cmd.equals("h")) displayInputMessage();
+            else if (cmd.equals("q")) break;
 
-            int month = getMonth(scanner);
-            if (check(month)) continue;
-
-            int days = cal.getNumOfDays(year, month);
-
-            cal.printCalendar(year, month, days);
         }
         System.out.println("Bye~");
+        scanner.close();
+    }
+
+    private void printCal(Scanner scanner, String delimeter, Calendar cal) {
+        List<Integer> dateList = new ArrayList();
+        String date = getDate(scanner);
+
+        String[] dateArr = split(delimeter, date);
+        parse(dateArr, dateList);
+
+        int year = dateList.get(0);
+        int month = dateList.get(1);
+        if (check(month)) return;
+        int day = dateList.get(2);
+
+        int days = cal.getNumOfDays(year, month);
+        cal.printCalendar(year, month, days);
+    }
+
+    private void cmdSearch() {
+    }
+
+    private void cmdRegister() {
+    }
+
+    private void displayInputMessage() {
+        System.out.println("+----------------------+");
+        System.out.println("| 1. 일정 등록 ");
+        System.out.println("| 2. 일정 검색 ");
+        System.out.println("| 3. 달력 보기");
+        System.out.println("| h. 도움말 q. 종료");
+        System.out.println("+----------------------+");
+    }
+
+    private void parse(String[] dateArr, List dateList) {
+        for (int i = 0; i < dateArr.length; i++) {
+            dateList.add(Integer.parseInt(dateArr[i]));
+        }
+    }
+
+    private String[] split(String delimeter, String date) {
+        String[] dateArr = date.split(delimeter);
+        return dateArr;
     }
 
 
@@ -67,16 +87,8 @@ public class Prompt {
         return false;
     }
 
-    private int getMonth(Scanner userInput) {
-        System.out.println("달을 입력하세요.");
-        System.out.print("MONTH> ");
-        return userInput.nextInt();
-    }
-
-
-    private int getYear(Scanner userInput) {
-        System.out.println("몇년도 달력인가요?(exit : -1)");
-        System.out.print("YEAR> ");
-        return userInput.nextInt();
+    private String getDate(Scanner userInput) {
+        System.out.println("년도를 입력하세요. ex : yyyy-mm-dd");
+        return userInput.next();
     }
 }
